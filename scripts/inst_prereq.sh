595c26db175f225c2a3b446ff88d38bd63d9919c
Ashutosh K Srivastava <oirtstut@gmail.com>
2026-07-15 15:43:57 +0530

    branding: added scripts.1

--- scripts/inst_prereq.sh
+++ scripts/inst_prereq.sh
@@ -1,41 +1,81 @@
 sudo apt update
 
+# sudo apt install -y \
+#     build-essential \
+#     autoconf \
+#     automake \
+#     pkg-config \
+#     git \
+#     cmake \
+#     gcc \
+#     g++ \
+#     make \
+#     flex \
+#     bison \
+#     re2c \
+#     libpcre2-dev \
+#     zlib1g-dev \
+#     libssl-dev \
+#     libevent-dev \
+#     libxml2-dev \
+#     libcurl4-openssl-dev \
+#     libssh2-1-dev \
+#     libsnmp-dev \
+#     libopenipmi-dev \
+#     libpq-dev \
+#     postgresql \
+#     postgresql-contrib \
+#     nginx \
+#     php-fpm \
+#     php-pgsql \
+#     php-gd \
+#     php-bcmath \
+#     php-mbstring \
+#     php-xml \
+#     php-ldap \
+#     php-zip \
+#     php-curl \
+#     php-intl \
+#     php-opcache \
+#     php-mysql \
+#     golang
+
+
 sudo apt install -y \
-    build-essential \
-    autoconf \
-    automake \
-    pkg-config \
     git \
-    cmake \
     gcc \
     g++ \
     make \
-    flex \
+    cmake \
+    pkg-config \
+    autoconf \
+    automake \
+    libtool \
     bison \
-    re2c \
-    libpcre2-dev \
-    zlib1g-dev \
-    libssl-dev \
-    libevent-dev \
-    libxml2-dev \
-    libcurl4-openssl-dev \
-    libssh2-1-dev \
-    libsnmp-dev \
-    libopenipmi-dev \
-    libpq-dev \
+    flex \
     postgresql \
     postgresql-contrib \
+    libpq-dev \
+    snmp \
+    snmpd \
+    libsnmp-dev \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    libxml2-dev \
+    libevent-dev \
+    libpcre2-dev \
+    libopenipmi-dev \
+    libssh-dev \
+    zlib1g-dev \
     nginx \
-    php-fpm \
-    php-pgsql \
-    php-gd \
-    php-bcmath \
-    php-mbstring \
-    php-xml \
-    php-ldap \
-    php-zip \
-    php-curl \
-    php-intl \
-    php-opcache \
-    php-mysql \
-    golang
+    php8.5 \
+    php8.5-fpm \
+    php8.5-pgsql \
+    php8.5-gd \
+    php8.5-bcmath \
+    php8.5-mbstring \
+    php8.5-xml \
+    php8.5-curl \
+    php8.5-ldap \
+    php8.5-zip \
+    golang-go
---
+++ scripts/manual-runs.md
@@ -0,0 +1,22 @@
+# Manually run the below code before running post-inst.sh
+
+ ./configure \
+     --prefix=/opt/coirtx \
+     --sysconfdir=/etc/coirtx \
+     --localstatedir=/var \
+     --enable-server \
+     --enable-agent2 \
+     --enable-webservice \
+     --with-postgresql \
+     --with-net-snmp \
+     --with-libcurl \
+     --with-openipmi \
+     --with-libxml2 \
+     --with-ssh2 \
+     --with-openssl
+
+     make -j$(nproc)
+
+     sudo make install
+
+     make dbschema
