8dfda7d02d57cabc8f57bf5f2b260d534e4aa088
Ashutosh K Srivastava <oirtstut@gmail.com>
2026-07-20 08:52:00 +0530

    refactor(ui): fixed php syntax error in CMenuItem

--- ui/include/classes/html/CMenuItem.php
+++ ui/include/classes/html/CMenuItem.php
@@ -89,7 +89,10 @@ class CMenuItem extends CTag {
 	 * @return CMenuItem
 	 */
 	public function setAction(string $action_name): self {
-		return $this->setUrl((new CUrl(CBrandHelper::FRONTEND_ENTRY)))->setArgument('action', $action_name), $action_name);
+		return $this->setUrl(
+			(new CUrl(CBrandHelper::FRONTEND_ENTRY))->setArgument('action', $action_name),
+			$action_name
+		);
 	}
 
 	/**
